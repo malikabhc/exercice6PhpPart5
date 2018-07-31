21db90061054a27369305cbbdde29ce48bca1d1d
Malikabhc <malika.bouhcine@gmail.com>
2018-07-31 09:08:58 +0200

    exercice6-5

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5/5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+// Création du tableau $region
+        $region = array (
+          '02' => 'Aisne',
+          // Se lit : Index 02 associé à Aisne.
+          '59' => 'Nord',
+          '60' => 'Oise',
+          '62' => 'Pas-de-Calais',
+          '80' => 'Somme');
+          echo $region['59'];
+      ?>
+    </p>
+  </body>
+</html>
